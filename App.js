5321b82ff95173e02411a83ce0365729e49f8a66
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-05 13:48:16 +0530

    Made a counter app

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    alert("mouted")
+    const timerID = setInterval(() => {
+      setCounter(counter=>counter+1);
+    }, 1000);
+  
+    return () => {
+      alert("unmounted")
+      clearInterval(timerID);
+    }
+  }, [])
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{counter}</Text>
       <StatusBar style="auto" />
     </View>
   );
