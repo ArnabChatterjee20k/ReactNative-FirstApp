f66525d27ce359ee67cffedd2bd1aa7839a8e344
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-05 20:20:06 +0530

    added vibration on pressing the button

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Pressable,Button } from 'react-native';
+import { StyleSheet, Text , Vibration, View , Pressable,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{borderWidth:2 , borderColor:"red" , padding:16}}>Open up App.js teo start working on your app!</Text>
       <Pressable style={styles.button} android_ripple={{ color: 'cyan' }}><Text style={{fontSize:16,padding:5}}>Button</Text></Pressable>
-      <Button title='hellow' style={styles.button}  color="#f194ff"/>
+      <Button title='Vibrate' style={styles.button}  color="#f194ff" onPress={()=>Vibration.vibrate(2*1000)}/>
     </View>
   );
 }
