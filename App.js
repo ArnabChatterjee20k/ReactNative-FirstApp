1d764a24f35da2949fa29ad23628dcd45dcb7903
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-08-05 20:31:06 +0530

    started a goal adding app

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text , Vibration, View , Pressable,Button } from 'react-native';
+import { StyleSheet, Text , TextInput, Vibration, View , Pressable,Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{borderWidth:2 , borderColor:"red" , padding:16}}>Open up App.js teo start working on your app!</Text>
-      <Pressable style={styles.button} android_ripple={{ color: 'cyan' }}><Text style={{fontSize:16,padding:5}}>Button</Text></Pressable>
-      <Button title='Vibrate' style={styles.button}  color="#f194ff" onPress={()=>Vibration.vibrate()}/>
+    <View>
+      <View>
+        <TextInput placeholder='enter your goal...'/>
+        <Button title='add'/>
+      </View>
+      <View>
+        <Text>List of goals....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button:{
-    borderColor: 'red',
-    borderWidth: 10,
-    borderRadius: 15 ,
-    width:100,
-    padding:10
-  }
 });
