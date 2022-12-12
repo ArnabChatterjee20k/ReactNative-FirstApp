3e43d439efcca5ee0c9ec8a00fe30c991b2a24b8
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-12 15:25:46 +0530

    added pressable style

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -1,21 +1,25 @@
-import { Text , Alert , Pressable,Vibration} from "react-native";
+import { Text, Alert, Pressable, Vibration } from "react-native";
 import { useGoalContext } from "../context/GoalContext";
 
 import styles from "../styles/style";
-export default function GoalItem({goal,id}) {
-  const {setGoals} = useGoalContext()
+export default function GoalItem({ goal, id }) {
+  const { setGoals } = useGoalContext();
   const showAlert = (data) => {
-    Vibration.vibrate(1000*1)
+    Vibration.vibrate(1000 * 1);
     Alert.alert(`ALert!`, `${data} is deleted`);
-    setGoals(prev=>prev.filter((e,i)=>i!==id))
+    setGoals((prev) => prev.filter((e, i) => i !== id));
   };
   return (
-    <Pressable onPress={()=>showAlert(goal)}>
-      <Text
-        style={styles.goalItem}
-      >
-        {goal}
-      </Text>
+    <Pressable
+      android_ripple={{ color: "white", foreground: true }}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? "red" : "white",
+        },
+      ]}
+      onPress={() => showAlert(goal)}
+    >
+      <Text style={styles.goalItem}>{goal}</Text>
     </Pressable>
   );
 }
