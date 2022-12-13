0fb117f4aaa2184f10c69f9f9bc3da27c8290e6b
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-13 08:18:41 +0530

    added native base

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import GoalInput from "./components/GoalInput";
 import GoalContext from "./context/GoalContext";
 import { View } from "react-native";
 import styles from "./styles/style";
+import { NativeBaseProvider } from "native-base";
+
 export default function App() {
   return (
-    <GoalContext>
-      <View style={styles.appContainer}>
-        <GoalInput/>
-        <GoalContainer/>
-      </View>
-    </GoalContext>
+    <NativeBaseProvider>
+      <GoalContext>
+        <View style={styles.appContainer}>
+          <GoalInput/>
+          <GoalContainer/>
+        </View>
+      </GoalContext>
+    </NativeBaseProvider>
   );
 }
